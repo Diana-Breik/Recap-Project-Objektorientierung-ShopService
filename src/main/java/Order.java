b07bbe68be3620a93680c5e4246ede1dc0549ef5
dianabreik <diana.sam341@gmail.com>
2023-09-12 21:43:47 +0200

    add the order time as an attribut for each order + test to validate the order time

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,5 +1,7 @@
 import lombok.With;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +9,8 @@ public record Order(
         String id,
         List<Optional<Product>> products,//
         @With
-        OrderStatus orderstatus
+        OrderStatus orderstatus,
+        LocalDateTime orderTime
 
 ) {
 
